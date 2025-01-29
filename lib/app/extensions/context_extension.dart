08b44714d76769eb5824bf153ef764fc0269b997
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2025-01-29 14:00:02 +0600

    base-functionality: view model get method added in context extension.

--- lib/app/extensions/context_extension.dart
+++ lib/app/extensions/context_extension.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:waadaa/app/base/base_view_model.dart';
 
 extension ContextExtension on BuildContext {
   AppLocalizations get localizations =>
       AppLocalizations.of(this) ??
       (throw Exception('Localizations not found.'));
+
+  VM getViewModel<VM extends BaseViewModel>() => read<VM>();
 }
