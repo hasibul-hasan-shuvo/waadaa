f506c705335096b21d556dbd658a540cc4f0c11e
AbedBS-23 <abedur.rahman@brainstation-23.com>
2025-02-03 20:30:01 +0600

    Updated slider indicator and added cached network image

--- lib/app/modules/home/view/home_page.dart
+++ lib/app/modules/home/view/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:core/assets/dimens.dart';
 import 'package:core/extensions/context_extension.dart';
@@ -8,7 +9,6 @@ import 'package:core/widgets/buttons/app_primary_button.dart';
 import 'package:core/widgets/buttons/app_secondary_button.dart';
 import 'package:core/widgets/primary_app_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:waadaa/app/base/base_page.dart';
 import 'package:waadaa/app/extensions/context_extension.dart';
 import 'package:waadaa/app/modules/home/viewmodel/home_state.dart';
@@ -169,8 +169,8 @@ class HomePage extends BasePage<HomeViewModel, HomeState> {
                 fontWeight: FontWeight.bold,
               ),
             ).paddingOnly(left: 8, top: 10, right: 5),
-            Image.network(
-              imageUrl, // Path to your image
+            CachedNetworkImage(
+              imageUrl: imageUrl, // Path to your image
               height: 200, // Optional: Adjust height
               fit: BoxFit.fill,
             ).paddingOnly(top: 7),
@@ -238,8 +238,9 @@ class HomePage extends BasePage<HomeViewModel, HomeState> {
           Container(
             color: Color(0xFFF7F5F2),
             padding: EdgeInsets.all(20),
-            child: Image.network(
-              "https://s3-alpha-sig.figma.com/img/add3/8874/c14a0cbb4cd437d6779e0a5a18963f63?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EMjRlDWiDiyPGKVql35KLJTc47Dn8b9F28pM7CKF4mWeCv0y~pzzeFC50~dKlMoJExBNqhR~KM8OH~65oIIv45sH7g7AdHU1y8ye9Gd0xjSeAamjxoBwbtSI5t9l4pkU8I918uCBoBVYuwTrfWjGYc63AqRW5og9r4qto~UwcuBl3j~JjldQQUwEfgmwSdtQfpikv8NwXaE0A~KQ1DL1Dus62eWzWT58qS2x-aS3uVVPRfzZezgP6p-yoEXiH33angbh2tH9DVGby~~1vUdD4zKA4znDyItE9jblQuxAKKJvmbsYQ2Mzkx1ohSTXJnHrK-BPg-pSNsqY25B34dB6uw__",
+            child: CachedNetworkImage(
+              imageUrl:
+                  "https://s3-alpha-sig.figma.com/img/add3/8874/c14a0cbb4cd437d6779e0a5a18963f63?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EMjRlDWiDiyPGKVql35KLJTc47Dn8b9F28pM7CKF4mWeCv0y~pzzeFC50~dKlMoJExBNqhR~KM8OH~65oIIv45sH7g7AdHU1y8ye9Gd0xjSeAamjxoBwbtSI5t9l4pkU8I918uCBoBVYuwTrfWjGYc63AqRW5og9r4qto~UwcuBl3j~JjldQQUwEfgmwSdtQfpikv8NwXaE0A~KQ1DL1Dus62eWzWT58qS2x-aS3uVVPRfzZezgP6p-yoEXiH33angbh2tH9DVGby~~1vUdD4zKA4znDyItE9jblQuxAKKJvmbsYQ2Mzkx1ohSTXJnHrK-BPg-pSNsqY25B34dB6uw__",
             ),
           ),
           SizedBox(height: 12),
@@ -316,8 +317,9 @@ class HomePage extends BasePage<HomeViewModel, HomeState> {
           Container(
             color: Color(0xFFF7F5F2),
             padding: EdgeInsets.all(4),
-            child: Image.network(
-              "https://s3-alpha-sig.figma.com/img/eae2/361c/c958aaaadc2d38f86dcb170a73cf843b?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=SD6~f34pDUqSx7kvm1hGSQZmr89iPCdTr0sw8ERFO1dStVfFXIuoxdNv2N3Z2SU9MqQV8bhZ8WZkro2qTypXq0UhP3I7FfNXe7L84YdszILHNY63a7xMheiBKHqSDlDEQBcfAmop8XI2x-8JhJPtiT8ond3prA6Ql1AReH8MAUpE9-HjdzbviuS1i7H-kSzsKXvz7qScUxmmFR8vKRgJATuOJiZuDxXmUBkz4s7d7Q-T4jk~U8~vQDXL4dTi21Yjl7uPti2qfkQI9cfjUeyiybZXOd4DEQ2yRoOKLYaafX9Rsy9eLktlrgdVbdnAxjK~GRix8iXewByMdvuKla~Euw__",
+            child: CachedNetworkImage(
+              imageUrl:
+                  "https://s3-alpha-sig.figma.com/img/eae2/361c/c958aaaadc2d38f86dcb170a73cf843b?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=SD6~f34pDUqSx7kvm1hGSQZmr89iPCdTr0sw8ERFO1dStVfFXIuoxdNv2N3Z2SU9MqQV8bhZ8WZkro2qTypXq0UhP3I7FfNXe7L84YdszILHNY63a7xMheiBKHqSDlDEQBcfAmop8XI2x-8JhJPtiT8ond3prA6Ql1AReH8MAUpE9-HjdzbviuS1i7H-kSzsKXvz7qScUxmmFR8vKRgJATuOJiZuDxXmUBkz4s7d7Q-T4jk~U8~vQDXL4dTi21Yjl7uPti2qfkQI9cfjUeyiybZXOd4DEQ2yRoOKLYaafX9Rsy9eLktlrgdVbdnAxjK~GRix8iXewByMdvuKla~Euw__",
             ),
           ),
           SizedBox(height: 12),
@@ -336,7 +338,7 @@ class HomePage extends BasePage<HomeViewModel, HomeState> {
   Widget singleBanner() {
     return InkWell(
       onTap: () {},
-      child: Image.network("https://i.imgur.com/FVvYyeT.png")
+      child: CachedNetworkImage(imageUrl: "https://i.imgur.com/FVvYyeT.png")
           .paddingOnly(top: 5, bottom: 10),
     );
   }
--- lib/app/modules/home/widgets/image_slider.dart
+++ lib/app/modules/home/widgets/image_slider.dart
@@ -1,7 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:core/extensions/widget_extension.dart';
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class ImageSlider extends StatefulWidget {
   const ImageSlider({super.key});
@@ -17,6 +16,21 @@ class _ImageSliderState extends State<ImageSlider> {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        ///10-2, 4:30-6:30
+        // SizedBox(
+        //   height: 220,
+        //   child: CarouselView(
+        //     scrollDirection: Axis.horizontal,
+        //     itemExtent: double.infinity,
+        //     itemSnapping: true,
+        //     children: List.generate(6, (int index) {
+        //       return Image.asset(
+        //         'assets/images/banner_home.png', // Path to your image
+        //         fit: BoxFit.cover,
+        //       );
+        //     }),
+        //   ),
+        // ),
         CarouselSlider(
           options: CarouselOptions(
             height: 220.0,
@@ -24,7 +38,7 @@ class _ImageSliderState extends State<ImageSlider> {
             viewportFraction: 1.0,
             initialPage: 0,
             enableInfiniteScroll: true,
-            reverse: true,
+            reverse: false,
             autoPlay: true,
             autoPlayInterval: Duration(seconds: 4),
             autoPlayAnimationDuration: Duration(milliseconds: 800),
@@ -40,26 +54,50 @@ class _ImageSliderState extends State<ImageSlider> {
           items: [1, 2, 3, 4, 5].map((i) {
             return Builder(
               builder: (BuildContext context) {
-                return Image.asset(
-                  'assets/images/banner_home.png', // Path to your image
-                  height: 200,
+                return CachedNetworkImage(
+                  imageUrl: 'https://i.imgur.com/DG5yU2k.png',
                   fit: BoxFit.cover,
                 );
               },
             );
           }).toList(),
         ),
-        AnimatedSmoothIndicator(
+        sliderIndicator(
           activeIndex: currentIndex,
           count: 5,
-          effect: ExpandingDotsEffect(
-            activeDotColor: Colors.black,
-            dotColor: Colors.white70,
-            dotHeight: 10,
-            dotWidth: 10,
-          ),
-        ).paddingSymmetric(vertical: 10),
+        ),
       ],
+    );
+  }
+
+  Widget sliderIndicator({required int activeIndex, required int count}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(count, (index) {
+        return Container(
+          width: 22.0,
+          height: 7.0,
+          margin: EdgeInsets.symmetric(vertical: 10.0),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: activeIndex == index ? Colors.black : Colors.white70,
+            borderRadius: BorderRadius.only(
+              topLeft: (activeIndex == index || index == 0)
+                  ? Radius.circular(8)
+                  : Radius.circular(0),
+              bottomLeft: (activeIndex == index || index == 0)
+                  ? Radius.circular(8)
+                  : Radius.circular(0),
+              topRight: (activeIndex == index || index == count - 1)
+                  ? Radius.circular(8)
+                  : Radius.circular(0),
+              bottomRight: (activeIndex == index || index == count - 1)
+                  ? Radius.circular(8)
+                  : Radius.circular(0),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
