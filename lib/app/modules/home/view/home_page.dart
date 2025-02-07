3f98634b0065c8f381a48f21c5fe955f1e03ba19
AbedBS-23 <abedur.rahman@brainstation-23.com>
2025-02-07 12:00:00 +0600

    Remove unnecessary code

--- lib/app/modules/home/view/home_page.dart
+++ lib/app/modules/home/view/home_page.dart
@@ -2,17 +2,13 @@ import 'dart:developer';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:core/extensions/widget_extension.dart';
-import 'package:core/widgets/buttons/app_primary_button.dart';
 import 'package:core/widgets/buttons/app_secondary_button.dart';
 import 'package:core/widgets/primary_app_bar.dart';
-import 'package:core/widgets/hero_banner_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:waadaa/app/base/base_page.dart';
 import 'package:waadaa/app/extensions/context_extension.dart';
 import 'package:waadaa/app/modules/home/viewmodel/home_state.dart';
 import 'package:waadaa/app/modules/home/viewmodel/home_view_model.dart';
-import 'package:waadaa/app/modules/home/widgets/home_counter_view.dart';
-import 'package:waadaa/app/modules/home/widgets/home_message_view.dart';
 import 'package:waadaa/app/modules/home/widgets/hero_banners_view.dart';
 import 'package:waadaa/app/modules/home/widgets/shop_by_section.dart';
 import 'package:waadaa/app/modules/home/widgets/welcome_reward_view.dart';
@@ -63,22 +59,6 @@ class HomePage extends BasePage<HomeViewModel, HomeState> {
           offerCategorySection(),
           singleBanner(),
           productListSection(title: "Recommended for you"),
-          // HomeMessageView(),
-          // HomeCounterView(),
-          // Center(
-          //   child: AppPrimaryButton(
-          //     title: "Increment",
-          //     onPressed: () =>
-          //         context.getViewModel<HomeViewModel>().increment(),
-          //   ),
-          // ),
-          // Center(
-          //   child: AppSecondaryButton(
-          //     title: "Update message",
-          //     onPressed: () =>
-          //         context.getViewModel<HomeViewModel>().updateMessage(),
-          //   ),
-          // ),
         ],
       ),
     );
