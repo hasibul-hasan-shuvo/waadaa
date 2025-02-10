b2b486e6b27e838d16ada6c24fbad9f33bb718c0
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2025-02-11 00:03:13 +0600

    feat(Bottom-navigation): NavigationBar top shadow added.

--- core/lib/widgets/bottom_navbar/bottom_navbar.dart
+++ core/lib/widgets/bottom_navbar/bottom_navbar.dart
@@ -1,3 +1,4 @@
+import 'package:core/assets/dimens.dart';
 import 'package:core/extensions/context_extension.dart';
 import 'package:core/widgets/bottom_navbar/models/bottom_navbar_item.dart';
 import 'package:flutter/material.dart';
@@ -16,21 +17,36 @@ class BottomNavbar<Identifier extends Enum> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-      labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
-      selectedIndex: _currentIndex,
-      onDestinationSelected: (index) {
-        if (_currentIndex != index) onItemSelect(items[index]);
-      },
-      destinations: items
-          .map((item) => item.toNavigationDestination(
-              _isItemSelected(item) ? context.colors.primary : Colors.grey))
-          .toList(),
+    return DecoratedBox(
+      decoration: _decoration,
+      child: NavigationBar(
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+        selectedIndex: _currentIndex,
+        onDestinationSelected: _onSelect,
+        destinations: items
+            .map((item) => item.toNavigationDestination(
+                _isItemSelected(item) ? context.colors.primary : Colors.grey))
+            .toList(),
+      ),
     );
+  }
+
+  void _onSelect(int index) {
+    if (_currentIndex != index) onItemSelect(items[index]);
   }
 
   int get _currentIndex => items.indexWhere((item) => _isItemSelected(item));
 
   bool _isItemSelected(BottomNavbarItem item) =>
       item.identifier == selectedItem.identifier;
+
+  BoxDecoration get _decoration => BoxDecoration(
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: Dimens.radiusLarge,
+            offset: Offset(0.0, 0.5),
+          )
+        ],
+      );
 }
