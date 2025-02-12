fe7f0f3c18b2e1aff19cbcfd2d82e76161959036
AbedBS-23 <abedur.rahman@brainstation-23.com>
2025-02-12 16:49:59 +0600

    feat(Home): api endpoints added

---
+++ data/lib/sources/remote/clients/end_points.dart
@@ -0,0 +1,11 @@
+import 'package:core/config/build_config.dart';
+
+class EndPoints {
+  EndPoints._();
+
+  static final adminBaseUrl = BuildConfig.instance.config.adminBaseUrl;
+  static final String heroBanners =
+      "$adminBaseUrl/api/configuration/hero-banners-config-list/";
+  static String categoryOffersCarousel =
+      "$adminBaseUrl/api/configuration/category-offer-carousel-config-list/";
+}
