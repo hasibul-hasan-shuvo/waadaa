bfc304b143a51b961376def2715c6b54db32d8e6
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2025-02-25 19:27:14 +0600

    feat(Product-Details): Category and SideMenu class created.

---
+++ domain/lib/models/category.dart
@@ -0,0 +1,19 @@
+import 'package:domain/models/side_menu.dart';
+
+class Category {
+  String id;
+  String name;
+  int level;
+  String slug;
+  List<Category> subcategories;
+  SideMenu? sideMenu;
+
+  Category({
+    required this.id,
+    required this.name,
+    required this.level,
+    required this.slug,
+    required this.subcategories,
+    this.sideMenu,
+  });
+}
