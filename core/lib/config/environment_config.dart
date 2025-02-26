9bf7f460aad535baae122f0fb3bb15eee505444d
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2025-02-27 00:23:01 +0600

    feat(Product-Details): adminBaseUrl added in EnvironmentConfig.

--- core/lib/config/environment_config.dart
+++ core/lib/config/environment_config.dart
@@ -1,9 +1,11 @@
 class EnvironmentConfig {
   final String appName;
   final String baseUrl;
+  final String? adminBaseUrl;
 
   EnvironmentConfig({
     required this.appName,
     required this.baseUrl,
+    required this.adminBaseUrl,
   });
 }
