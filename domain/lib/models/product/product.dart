82d16543595e38d04ee7bdf2bd48f82a64952240
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2025-02-26 21:31:21 +0600

    feat(Product-Details): Product domain model class implemented.

---
+++ domain/lib/models/product/product.dart
@@ -0,0 +1,46 @@
+import 'package:domain/models/category/category.dart';
+import 'package:domain/models/product/product_image.dart';
+import 'package:domain/models/product/product_variant.dart';
+import 'package:domain/models/product/variants.dart';
+
+class Product {
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.isOverseas,
+    required this.productType,
+    required this.manufacturer,
+    required this.warrantyPolicy,
+    required this.returnPolicy,
+    required this.usagePrecaution,
+    required this.categories,
+    required this.brand,
+    required this.approvalStatus,
+    required this.slug,
+    required this.shippingInformation,
+    required this.variants,
+    required this.averageRating,
+    required this.images,
+    required this.variantMappings,
+  });
+
+  final String id;
+  final String name;
+  final String description;
+  final bool isOverseas;
+  final String productType;
+  final String manufacturer;
+  final String warrantyPolicy;
+  final String returnPolicy;
+  final String usagePrecaution;
+  final List<Category> categories;
+  final String brand;
+  final String approvalStatus;
+  final String slug;
+  final String shippingInformation;
+  final List<Variants> variants;
+  final int averageRating;
+  final List<ProductImage> images;
+  final List<ProductVariant> variantMappings;
+}
---
+++ domain/lib/models/product/product_variant.dart
@@ -0,0 +1,33 @@
+import 'package:domain/models/product/product_variant_attribute.dart';
+
+class ProductVariant {
+  ProductVariant({
+    required this.id,
+    required this.attributeValues,
+    required this.waadaaSku,
+    required this.currency,
+    required this.retailPrice,
+    required this.description,
+    required this.estimatedDeliveryTime,
+    required this.isWaadaaApproved,
+    required this.initialStockQuantity,
+    required this.availableStockQuantity,
+    required this.masterProduct,
+    required this.attributeValue,
+    required this.colorImages,
+  });
+
+  final String id;
+  final List<ProductVariantAttribute> attributeValues;
+  final String waadaaSku;
+  final String currency;
+  final int retailPrice;
+  final String description;
+  final int estimatedDeliveryTime;
+  final bool isWaadaaApproved;
+  final int initialStockQuantity;
+  final int availableStockQuantity;
+  final String masterProduct;
+  final List<String> attributeValue;
+  final List<String> colorImages;
+}
