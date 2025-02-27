f3ac58e5bc4c7b1b359743116e07fc9f3968389e
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2025-02-27 23:13:38 +0600

    feat(Product-Details): getProductById is implemented and variants mapping added.

--- domain/lib/models/product/product_with_variants_map_table.dart
+++ domain/lib/models/product/product_with_variants_map_table.dart
@@ -1,11 +1,14 @@
 import 'package:domain/models/product/product.dart';
+import 'package:domain/models/product/product_variant.dart';
 
 class ProductWithVariantsMapTable {
   ProductWithVariantsMapTable({
     required this.product,
+    required this.colorImages,
     required this.variantsMapTable,
   });
 
   final Product product;
-  final Map variantsMapTable;
+  final Map<String, List<String>> colorImages;
+  final Map<String, List<ProductVariant>> variantsMapTable;
 }
--- domain/lib/usecases/product_by_id_use_case.dart
+++ domain/lib/usecases/product_by_id_use_case.dart
@@ -1,4 +1,5 @@
 import 'package:di/di.dart';
+import 'package:domain/models/product/product_variant.dart';
 import 'package:domain/models/product/product_with_variants_map_table.dart';
 import 'package:domain/repositories/product_repository.dart';
 
@@ -9,11 +10,39 @@ class ProductByIdUseCase {
   ProductByIdUseCase(this._productRepository);
 
   Future<ProductWithVariantsMapTable> getProductById(String id) async {
-    return _productRepository.getProductById(id).then((product) {
-      return ProductWithVariantsMapTable(
-        product: product,
-        variantsMapTable: {},
-      );
-    });
+    final product = await _productRepository.getProductById(id);
+
+    final Map<String, List<ProductVariant>> variantsMapTable = {};
+    final Map<String, List<String>> colorImages = {};
+
+    _processVariantMappings(
+      variantsMapTable: variantsMapTable,
+      colorImages: colorImages,
+      variantMappings: product.variantMappings,
+    );
+
+    return ProductWithVariantsMapTable(
+      product: product,
+      colorImages: colorImages,
+      variantsMapTable: variantsMapTable,
+    );
+  }
+
+  void _processVariantMappings({
+    required Map<String, List<ProductVariant>> variantsMapTable,
+    required Map<String, List<String>> colorImages,
+    required List<ProductVariant> variantMappings,
+  }) {
+    for (final variant in variantMappings) {
+      for (final attributeValue in variant.attributeValues) {
+        final value = attributeValue.value;
+
+        variantsMapTable.putIfAbsent(value, () => []).add(variant);
+
+        if (attributeValue.name.toLowerCase() == 'color') {
+          colorImages.putIfAbsent(value, () => variant.colorImages);
+        }
+      }
+    }
   }
 }
